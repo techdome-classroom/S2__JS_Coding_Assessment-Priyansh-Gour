d8e57e3c79ea0504e3903626cdf0f5e5fea4c345
Priyansh-Gour <priyanshgour817@gmail.com>
2024-09-25 14:24:44 +0530

    completed both questions

--- program1.js
+++ program1.js
@@ -3,7 +3,28 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (map[char]) {
+            let topElement = stack.length > 0 ? stack.pop() : '#';
+
+            if (topElement !== map[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,8 +3,36 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
     
+    let result = 0;
+    
+    
+    for (let i = 0; i < s.length; i++) {
+        
+        let currentValue = romanMap[s[i]];
+    
+        let nextValue = romanMap[s[i + 1]];
+
+        
+        if (currentValue < nextValue) {
+            result -= currentValue;
+        } else {
+            result += currentValue;
+        }
+    }
+    
+    return result;
 };
 
 
-module.exports={romanToInt}
+module.exports = { romanToInt };
